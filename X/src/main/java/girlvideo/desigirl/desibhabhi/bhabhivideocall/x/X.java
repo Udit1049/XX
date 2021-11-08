c9a9cf2da3a82df9971df168db9b571c9b03d4dc
Udit1049 <uditpatel194@gmail.com>
2021-11-08 15:39:33 +0530

    Initial commit

---
+++ X/src/main/java/girlvideo/desigirl/desibhabhi/bhabhivideocall/x/X.java
@@ -0,0 +1,11 @@
+package girlvideo.desigirl.desibhabhi.bhabhivideocall.x;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class X {
+
+    public static  void Simpletost(Context context, String Message){
+        Toast.makeText(context, Message, Toast.LENGTH_SHORT).show();
+    }
+}
